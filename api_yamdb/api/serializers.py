52b9b8bc633e5c5e85f14d1b0483972d15d6bdc5
Павел Данелия <daneliyapavel@yandex.ru>
2022-04-19 20:56:33 +0300

    First try

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import authenticate
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 from rest_framework_simplejwt.tokens import RefreshToken
